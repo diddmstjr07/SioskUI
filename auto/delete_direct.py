b1efd2219b5c0019548654d21f91eb017fcb57d7
diddmstjr07 <diddmstjr071121@gmail.com>
2024-07-06 19:19:41 +0900

    version 4.4 (SioskServer.. Windows version tunning)

--- auto/delete_direct.py
+++ auto/delete_direct.py
@@ -1,8 +1,9 @@
 import os
 
 def delete_dot_underscore_files():
-    directory = os.getcwd()
-    for root, dirs, files in os.walk(directory):
+    parent_directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    
+    for root, dirs, files in os.walk(parent_directory):
         for file in files:
             if file.startswith("._"):
                 file_path = os.path.join(root, file)
@@ -11,3 +12,5 @@ def delete_dot_underscore_files():
                     print(f"Deleted: {file_path}")
                 except Exception as e:
                     print(f"Error deleting {file_path}: {e}")
+
+delete_dot_underscore_files()
